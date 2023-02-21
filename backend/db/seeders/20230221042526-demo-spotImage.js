039a6a2eaafd05c4c25471986a755d3d8ff1fef4
DLloyd <dlloyd1992@gmail.com>
2023-02-20 20:59:46 -0800

    updated seeders

--- backend/db/seeders/20230221042526-demo-spotImage.js
+++ backend/db/seeders/20230221042526-demo-spotImage.js
@@ -6,18 +6,10 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
-options.tableName = 'SpotImage';
+options.tableName = 'SpotImages';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+
     return queryInterface.bulkInsert(options, [
       {
         spotId: 1,
@@ -83,12 +75,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
       spotId: { [Op.in]: [1, 2, 3, 4] }
