9e3e320cbb8c605f4d69a542962be92fe3755368
DLloyd <dlloyd1992@gmail.com>
2023-02-20 21:39:17 -0800

    updated Review seed

--- backend/db/seeders/20230221050308-demo-review.js
+++ backend/db/seeders/20230221050308-demo-review.js
@@ -118,7 +118,7 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1, 2, 3] }
+      spotId: { [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] }
     })
   }
 };
