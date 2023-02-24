3d6e94291d8765eec1b71150e910c40198fe3da3
DLloyd <dlloyd1992@gmail.com>
2023-02-23 16:07:18 -0800

    review feature complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-const spotsRouter = require('./spots.js')
+const spotsRouter = require('./spots.js');
+const previewsRouter = require('./reviews.js');
 const { restoreUser } = require("../../utils/auth.js");
 
 router.use(restoreUser);
@@ -9,6 +10,7 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
+router.use('/reviews', previewsRouter);
 
 
 router.post('/test', function(req, res) {
